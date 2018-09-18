f312372da9e53540b5f1d0bf3d6a99e3f7648a3f
vikkigowda <43372146+vikkigowda@users.noreply.github.com>
2018-09-18 16:07:02 +0530

    Update new.c

--- new.c
+++ new.c
@@ -3,12 +3,11 @@ int main()
 {
     int n, i, flag = 0;
 
-    printf("Enter a positive integer: ");
+    printf("Enter integer: ");
     scanf("%d",&n);
 
     for(i=2; i<=n/2; ++i)
     {
-        // condition for nonprime number
         if(n%i==0)
         {
             flag=1;
